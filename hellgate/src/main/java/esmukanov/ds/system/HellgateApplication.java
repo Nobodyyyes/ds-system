1583d4b83e10602349ce422f81604b1c2d715b1d
esmukanov <esmukanov>
2025-10-13 17:04:32 +0600

    first commit

---
+++ hellgate/src/main/java/esmukanov/ds/system/HellgateApplication.java
@@ -0,0 +1,11 @@
+package esmukanov.ds.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellgateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HellgateApplication.class, args);
+    }
+}
